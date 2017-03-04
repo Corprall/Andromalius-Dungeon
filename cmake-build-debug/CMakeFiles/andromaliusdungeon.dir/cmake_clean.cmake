ebfde86e3e74bb9e18adfa5a3196d1b85eecca84
Daniel Smith <Daniel Smith>
2017-03-04 20:37:58 +1300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/andromaliusdungeon.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/andromaliusdungeon.dir/main.cpp.obj"
+  "andromaliusdungeon.pdb"
+  "andromaliusdungeon.exe"
+  "andromaliusdungeon.exe.manifest"
+  "libandromaliusdungeon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/andromaliusdungeon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
